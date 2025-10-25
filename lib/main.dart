51a4b03106cbc6ae06990e2ed04e71c5d18342a2
Edson <59818164+AnJayAx@users.noreply.github.com>
2025-10-25 23:54:50 +0800

    working with portrait and navbar

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Landmark Detector',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: const ScanScreen(),
+      home: const MainScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
